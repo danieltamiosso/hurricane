9d4d6567886ebcf3619369e5e0bd9abb5454d84e
Daniel Tamiosso <email@danieltamiosso.com>
2010-07-23 16:57:09 -0300

    Commit

---
+++ lib/hurricane.rb
@@ -0,0 +1,41 @@
+require 'hpricot'
+
+class Hurricane
+	def self.from(file)
+		blog = Blog.new	
+		doc = Hpricot::XML(file)
+		(doc/:channel).each do |info|
+			blog.title = (info/:title)[0].inner_html
+			blog.description = (info/:description).inner_html
+			blog.link = (info/:link)[0].inner_html
+			blog.created_at = (info/'pubDate')[0].inner_html
+
+			(info/'wp:category'/'wp:category_nicename').each do |category|
+				blog.categories << category.inner_html
+			end
+			
+			(info/:item).each do |item|				
+				post = Post.new
+				post.title = (item/:title).inner_html
+				post.link = (item/:link).inner_html
+				post.created_at = (item/'pubDate').inner_html
+				post.description = (item/'content:encoded').inner_html.gsub('<![CDATA[', '').gsub(']]>', '')
+				blog.posts << post
+			end
+
+		end
+		blog
+  	end
+end
+
+class Blog
+	def initialize
+		@categories = Array.new
+		@posts = Array.new
+	end
+	attr_accessor :title, :link, :description, :created_at, :categories, :posts
+end
+
+class Post
+	attr_accessor :title, :link, :created_at, :description
+end
--- spec/hurricane_spec.rb
+++ spec/hurricane_spec.rb
@@ -1,7 +1,51 @@
 require 'spec_helper'
 
-describe "Hurricane" do
-  it "fails" do
-    should.flunk "hey buddy, you should probably rename this file and start specing for real"
-  end
+describe 'Hurricane' do
+	
+	FILE = File.open('./wordpress.2010-07-12.xml', 'r')
+	@blog = Hurricane.from(FILE)
+
+	it 'should be offer blog title' do
+		@blog.title.should.equal 'Daniel Tamiosso'
+	end
+	
+	it 'should be offer blog link' do
+		@blog.link.should.equal 'http://danieltamiosso.com'
+	end
+	
+	it 'should be offer blog description' do
+		@blog.description.should.equal 'Only me'
+	end
+	
+	it 'should be offer blog created date' do
+		@blog.created_at.should.equal 'Wed, 15 Jul 2009 23:32:34 +0000'
+	end
+	
+	it 'should be offer blog categories' do		
+		@blog.categories.should.include? 'agil'
+		@blog.categories.should.include? 'boas-praticas'
+		@blog.categories.should.include? 'desenvolvimento'
+		@blog.categories.should.include? 'design'
+	end	
+	
+	it 'should be offer post list' do		
+		@blog.posts.size.should.equal 2
+	end	
+	
+	it 'should be get a post with title' do		
+		@blog.posts.first.title.should.equal 'Ou uma coisa ou outra: Command and Query Separation'
+	end	
+	
+	it 'should be get a post with link' do			
+		@blog.posts.first.link.should.equal 'http://danieltamiosso.com/2009/07/09/ou-uma-coisa-ou-outra-command-and-query/'
+	end
+	
+	it 'should be get a post with created date' do
+		@blog.posts.first.created_at.should.equal 'Thu, 09 Jul 2009 16:50:14 +0000'
+	end
+	
+	it 'should be get a post with description' do
+		@blog.posts.first.description.should.equal 'Texto do post.'
+	end
+		
 end
